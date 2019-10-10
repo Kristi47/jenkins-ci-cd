7ed8b766dbefd9d4f2022a32682333aa8af54a66
Kristi Mitta <kristimita33@gmail.com>
2019-10-10 10:00:20 -0700

    Fixing deploy bash script

--- unit-test.js
+++ unit-test.js
@@ -1,6 +1,6 @@
 
 const expect = require('expect');
-const {toLowerCase} = require('./utils')
+const {toLowerCase, sum} = require('./utils')
 
 describe("LowerCase Validation",()=>{
 
@@ -9,5 +9,12 @@ describe("LowerCase Validation",()=>{
         let str = "KRISTI";
         let testStr = toLowerCase(str);
         expect(testStr).toBe("kristi")
+    });
+
+    it("add two numbders",() =>{
+        let a = 5;
+        let b = 3;
+        let sum1 = sum(a, b);
+        expect(sum1).toBe(8)
     });
 });
