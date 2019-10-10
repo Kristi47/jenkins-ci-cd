4d18c92be2b46c56967aa52c7c7efd302f967463
Kristi Mita <42234294+Kristi47@users.noreply.github.com>
2019-10-10 14:58:55 +0200

    Testing Jenkins Github Pull Request (#1)
    * Testing Jenkins Github Pull Request
    
    * Testing Jenkins Github Pull Request
    
    * Testing Jenkins Github Pull Request
    
    * Testing Jenkins Github Pull Request
    
    * Testing Jenkins Github Pull Request
    
    * Jenkins CI/CD

--- utils.js
+++ utils.js
@@ -6,4 +6,25 @@ let sum = (a , b) => {
     return a+b
 }
 
-module.exports = {toLowerCase, sum}
+
+let multiply = (a , b) => {
+    return a*b
+}
+
+let test = (a , b) => {
+    return a*b
+}
+
+let test2 = (a , b) => {
+    return a*b
+}
+
+let test3 = (a , b) => {
+    return a*b
+}
+
+let test4 = (a , b) => {
+    return a*b
+}
+
+module.exports = {toLowerCase, sum, multiply, test}
